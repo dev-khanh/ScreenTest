791f20964e516621810da754189cb4f83d7e225c
Duong Quoc Khanh <devdqkhanh@gmail.com>
2020-03-05 16:46:00 +0700

    layout test

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import {AppRegistry} from 'react-native';
 import App from './src/components/App';
+
+// import LoginButtom from './test/LoginButtom';
 import {name as appName} from './app.json';
 AppRegistry.registerComponent(appName, () => App);
